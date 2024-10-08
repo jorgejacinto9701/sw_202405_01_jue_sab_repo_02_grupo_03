edf6c498653d567d06a49ebdde4453207cdfa622
Adeesly <past.21.01@gmail.com>
2024-10-08 15:47:16 -0500

    fin

--- src/main/java/com/example/dsw/controller/BogotaController.java
+++ src/main/java/com/example/dsw/controller/BogotaController.java
@@ -1,10 +1,44 @@
 package com.example.dsw.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dsw.entity.Bogota;
+import com.example.dsw.service.BogotaService;
 
 @RestController
 @RequestMapping("/url/bogota")
 public class BogotaController {
+
+     private  BogotaService bogotaService;
+
+     public BogotaController(BogotaService bogotaService) {
+        this.bogotaService = bogotaService;
+    }
+
+     @GetMapping("/listarPorIdYNombre")
+     public List<Bogota> listarPorIdYNombre(@RequestParam Integer id, @RequestParam String nombre) {
+         return bogotaService.listarPorIdYNombre(id, nombre);
+     }
+
+     @GetMapping("/listarPorRangoDeLatitud")
+     public List<Bogota>listarPorRangoDeLatitud(@RequestParam double minLatitud,@RequestParam double maxLatitud) {
+         return bogotaService.listarPorRangoDeLatitud(minLatitud,maxLatitud);
+     }
+
+     @GetMapping("/iconografia")
+     public List<Bogota> buscarPorIconografia(@RequestParam String iconografia) {
+        return bogotaService.buscarPorIconografia(iconografia);
+    }
+
+    @GetMapping("/buscarPorIdOTelefono")
+    public List<Bogota> buscarPorIdOTelefono(@RequestParam Integer objectId, @RequestParam String telefono) {
+    return bogotaService.listarPorIdOTelefono(objectId, telefono);
+}
+     
 
 }
--- src/main/java/com/example/dsw/service/BogotaService.java
+++ src/main/java/com/example/dsw/service/BogotaService.java
@@ -1,5 +1,18 @@
 package com.example.dsw.service;
 
+import java.util.List;
+
+import com.example.dsw.entity.Bogota;
+
 public interface BogotaService {
+
+     public List<Bogota> listarPorIdYNombre(Integer id, String nombre);
+
+     public List<Bogota> listarPorRangoDeLatitud(double minLatitud, double maxLatitud);
+
+     public List<Bogota> buscarPorIconografia(String iconografia);
+
+     public List<Bogota> listarPorIdOTelefono(Integer objectId, String telefono);
+
 
 }
