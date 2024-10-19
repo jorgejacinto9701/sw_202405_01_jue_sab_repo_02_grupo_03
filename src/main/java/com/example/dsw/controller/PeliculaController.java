0f5b33f417140d4f66c800d45f3a5fef35cb38bb
PowerST2 <diegostd99@gmail.com>
2024-10-19 18:28:20 -0500

    T3-DAVILA

--- src/main/java/com/example/dsw/controller/PeliculaController.java
+++ src/main/java/com/example/dsw/controller/PeliculaController.java
@@ -3,6 +3,7 @@ package com.example.dsw.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.dsw.entity.Pelicula;
 import com.example.dsw.service.PeliculaService;
@@ -16,15 +17,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PeliculaController {
 
     @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
     private PeliculaService peliculaService;
 
     @GetMapping("/listaporPaisOGenero/{country}/{genre}")
     public List<Pelicula> listaporPaisOGenero(@PathVariable String country, @PathVariable String genre) {
+        restTemplate.postForObject("http://localhost:8097/url/auditoria/registrar/MONGODB-Examen-Pelicula", null, String.class);
         return peliculaService.listaporPaisOGenero(country, genre);
     }
 
     @GetMapping("/buscarPeliculaEntreAnos/{anoInicio}/{anoFinal}")
     public List<Pelicula> buscarPeliculaEntreAnos(@PathVariable Integer anoInicio, @PathVariable Integer anoFinal) {
+        restTemplate.postForObject("http://localhost:8097/url/auditoria/registrar/MONGODB-Examen-Pelicula", null, String.class);
         return peliculaService.buscarPeliculaEntreAnos(anoInicio, anoFinal);
     }
 }
